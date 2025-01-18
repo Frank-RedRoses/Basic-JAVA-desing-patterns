1285fa0940123d3d9c0f4d36e629d10b880ea9ec
Frank-RedRoses <113691934+Frank-RedRoses@users.noreply.github.com>
2025-01-18 17:22:20 +0100

    feat: Basic framework for a Swinger application that uses MVC pattern.

---
+++ MVC/src/com/caveofprogramming/designpattern/demo1/Application.java
@@ -0,0 +1,44 @@
+package com.caveofprogramming.designpattern.demo1;
+
+import com.caveofprogramming.designpattern.demo1.controller.Controller;
+import com.caveofprogramming.designpattern.demo1.model.Model;
+import com.caveofprogramming.designpattern.demo1.view.View;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * This application is an example of the Model-View-Controller (MVC) design pattern.
+ * It is intended to demonstrate how the MVC pattern is valuable for large applications.
+ * <br>
+ * For smaller applications, this pattern may not be always necessary. In some cases,
+ * the combination of just the View and the Model might suffice, and the Controller may
+ * not be needed. However,separating the front end (View) from the data in the back end
+ * (Model) remains essential.<br>
+ * For this application let's suppose we have a Swing application that has some complexity;
+ * most games would fall into this category.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        // This block of code from here is specific to Swing and not MVC pattern related.
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                runApp();  // This is the entry point of our application
+            }
+        });
+    }
+
+    // Inside this method we make use of the MVC pattern. The method's name does not matter.
+    private static void runApp() {
+        Model model = new Model();
+        View view = new View(model);    // The model will listen to the view
+        Controller controller = new Controller(view, model);
+        /* The controller send commands to the View and the Model, and it is almost certainly
+         * listening to the View, but may or may not listen to the Model. So, this code will
+         * probably be written as it listen to the model.
+         */
+
+        // To do: Implement the observer pattern to make the Controller listen to the View
+    }
+}
---
+++ MVC/src/com/caveofprogramming/designpattern/demo1/view/View.java
@@ -0,0 +1,27 @@
+package com.caveofprogramming.designpattern.demo1.view;
+
+import com.caveofprogramming.designpattern.demo1.model.Model;
+
+import javax.swing.JFrame;
+
+/**
+ * This class represent the view of our application. You would not necessarily call
+ * this class {@code View} nor would the root package necessarily be called {@code view}.
+ * It is often more appropriate to give this class a name such as {@code MainFrame}.
+ * The important concept is that this is the part of the code responsible for interacting
+ * with the user.
+ */
+public class View extends JFrame {
+
+    private final Model model;
+
+    /**
+     * The {@code View} constructor receives a reference to the {@code Model}
+     * and that allows the {@code Model} to listen to the {@code View}.
+     * @param model
+     */
+    public View(Model model) {
+        super("MVC Demo");  // The parent constructor (JFrame) accepts a title for the app.
+        this.model = model;
+    }
+}
