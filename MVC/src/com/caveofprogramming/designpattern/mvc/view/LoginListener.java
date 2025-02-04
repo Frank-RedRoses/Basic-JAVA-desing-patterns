b6d536103f696a148c3b4099463d3422d43d7782
Frank-RedRoses <113691934+Frank-RedRoses@users.noreply.github.com>
2025-02-04 17:43:09 +0100

    refactor: Changes the create-user application source folder from the main folder to the MVC folder.
    - refactor: Moves all classes and tests of the Create-User application to the MVC folder to continue the development in it.
    - This updated application represents a better example to the MVC pattern compared to the previous version stored in the MVC folder.
    - The `src` directory in the main folder will now be used for developing new patterns.

--- MVC/src/com/caveofprogramming/designpattern/mvc/view/LoginListener.java
+++
@@ -1,9 +0,0 @@
-package com.caveofprogramming.designpattern.mvc.view;
-
-/**
- * This interface must be implemented by the Login Observer class.
- * It declares the loginPerform() method.
- */
-public interface LoginListener {
-    public void loginPerform(LoginFormEvent event);
-}
